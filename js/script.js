3f90bc326d6370de97e37f2e61c08edec97e5b7e
FrancescoM95 <mon95fran@gmail.com>
2024-01-23 09:42:10 +0100

    completed slider

--- js/script.js
+++ js/script.js
@@ -5,8 +5,35 @@ const { createApp } = Vue;
 const app = createApp({
     name: 'VueCarousel',
     data: () => ({
-        pictures
-    })
+        pictures,
+        currentIndex: 0
+    }),
+    computed: {
+        lastElementIndex() {
+            return this.pictures.length - 1
+        },
+
+        isFirstIndex() {
+            return this.currentIndex === 0;
+        },
+
+        isLastIndex() {
+            return this.currentIndex === this.lastElementIndex;
+        }
+    },
+    methods: {
+        setCurrentIndex(target) {
+            if (target === 'next') {
+                if (this.isLastIndex) this.currentIndex = 0;
+                else this.currentIndex++;
+            } else if (target === 'prev') {
+                if (this.isFirstIndex) this.currentIndex = this.lastElementIndex;
+                else this.currentIndex--;
+            } else {
+                this.currentIndex = target;
+            }
+        }
+    }
 });
 
 app.mount('#root');
